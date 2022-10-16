b14e24b297ee75062721f861b675d2674b21cd12
JohnsonDaniyan <toyinjohnsondaniyan.td@gmail.com>
2022-10-16 23:24:05 +0100

    settingUp!!!

---
+++ pages/inproject.js
@@ -0,0 +1,6 @@
+import logo from '../images/logoPPT.png'
+import Image from 'next/image'
+const Inproject = () => {
+    return <div className="splash h-screen flex justify-center items-center" ><Image className="w-{321.81px}" src={logo} alt="" /></div>
+}
+export default Inproject
