8892895034a249ec18a195700c5647b71af8c52e
JohnsonDaniyan <toyinjohnsondaniyan.td@gmail.com>
2022-12-13 10:13:43 +0100

    fix

--- pages/index.js
+++ pages/index.js
@@ -6,13 +6,7 @@ import headerbg from '../images/headerbg.png'
 import Link from 'next/link'
 
 
-const cors = require('cors');
-const corsOptions ={
-    origin:'http://localhost:3000', 
-    credentials:true,            //access-control-allow-credentials:true
-    optionSuccessStatus:200
-}
-app.use(cors(corsOptions));
+
 
 const projects = [
   {
