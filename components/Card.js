55e1011fcf4027ea35af984f28db8c7b37b0d2ca
JohnsonDaniyan <toyinjohnsondaniyan.td@gmail.com>
2022-12-14 05:50:49 +0100

    Update Card.js

--- components/Card.js
+++ components/Card.js
@@ -8,11 +8,11 @@ function Card(props) {
     console.log(props)
   return (
     <Link href={`/project/${props.name}`} >
-        <div className='cursor-pointer mb-10 flex gap-10 items-center bg-white w-fill p-5 border-black border-solid border rounded-lg relative'>
+        <div className='cursor-pointer mb-10 flex items-center bg-white w-fill p-5 border-black border-solid border rounded-lg relative'>
             
             <Image width={100} height={100} style={{position:"relative",height:"100%"}} className='relative self-center object-cover' src={props.img} alt="" />
             
-            <div className='border-black text-left' >
+            <div className='border-black ml-5 pl-3 border-l-2 text-left' >
                 <h1 className='font-bold text-sm'>
                     {props.name}
                 </h1>
