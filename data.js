a7b2256f08a8ef0c5991e273c662a1b2ab617dc2
JohnsonDaniyan <toyinjohnsondaniyan.td@gmail.com>
2022-12-14 05:47:42 +0100

    4projects

--- data.js
+++ data.js
@@ -1,30 +1,3 @@
-// export const projects = {
-//   "Vaccine Cooling System":{
-//       "sub":"(vcs)",
-//       "desc":"",
-//       "link":"https://d2zes9ifb0ot5n.cloudfront.net/vcs.glb",
-//       "img":"https://innov8-ar-bucket.s3.amazonaws.com/Images/Egg+Incubator.png"
-//     },
-//     "Mobility":{
-//         "sub":"Mobility",
-//         "desc":"Description here for Mob",
-//         "link":"https://d2zes9ifb0ot5n.cloudfront.net/Mobility.glb",
-//         "img":"https://d2zes9ifb0ot5n.cloudfront.net/Mobility.png"
-//     },
-//     "Egg Incubator":{
-//         "sub":"Egg Incubator",
-//         "desc":"Description here for EI",
-//         "link":"https://d2zes9ifb0ot5n.cloudfront.net/Egg+incubator1.glb",
-//         "img":"https://d2zes9ifb0ot5n.cloudfront.net/Egg+incubator.png"
-//     },
-//     "Mushroom Farm":{
-//       "sub":"Mushroom Farm",
-//       "desc":"Description here for MF",
-//       "link":"https://d2zes9ifb0ot5n.cloudfront.net/mushroom+farm4.glb",
-//       "img":"https://d2zes9ifb0ot5n.cloudfront.net/Mushroom+Farm.png"
-//     },
-// }
-
 
  export const projects = [
     {
